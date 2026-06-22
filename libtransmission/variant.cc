fdbeaa3086f7fc52ee3e0cf4dccd951d92f236a8
Charles Kerr <charles@charleskerr.com>
2026-06-22 11:25:13 -0500

    refactor: remove unused tr_variantInitList()

--- libtransmission/variant.cc
+++ libtransmission/variant.cc
@@ -321,13 +321,6 @@ bool tr_variantDictFindDict(tr_variant* const var, tr_quark key, tr_variant** se
 
 // ---
 
-void tr_variantInitList(tr_variant* initme, size_t n_reserve)
-{
-    auto vec = tr_variant::Vector{};
-    vec.reserve(n_reserve);
-    *initme = std::move(vec);
-}
-
 void tr_variantInitDict(tr_variant* initme, size_t n_reserve)
 {
     *initme = tr_variant::Map{ n_reserve };
