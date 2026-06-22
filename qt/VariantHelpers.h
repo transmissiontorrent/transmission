a71d02d6b80e324b38ba20e49855213c29ca977b
Charles Kerr <charles@charleskerr.com>
2026-06-22 10:53:41 -0500

    refactor: remove unused getValue() overloads

--- qt/VariantHelpers.h
+++ qt/VariantHelpers.h
@@ -43,109 +43,13 @@ namespace trqt::variant_helpers
 {
 template<typename T>
 auto getValue(tr_variant const* variant)
-    requires std::is_same_v<T, bool>
-{
-    std::optional<T> ret;
-
-    if (auto value = T{}; tr_variantGetBool(variant, &value))
-    {
-        ret = value;
-    }
-
-    return ret;
-}
-
-template<typename T>
-auto getValue(tr_variant const* variant)
-    requires std::is_same_v<T, int64_t> || std::is_same_v<T, uint64_t> || std::is_same_v<T, int> || std::is_same_v<T, time_t>
+    requires std::is_same_v<T, int>
 {
     std::optional<T> ret;
 
     if (auto value = int64_t{}; tr_variantGetInt(variant, &value))
     {
         ret = value;
-    }
-
-    return ret;
-}
-
-template<typename T>
-auto getValue(tr_variant const* variant)
-    requires std::is_same_v<T, double>
-{
-    std::optional<T> ret;
-
-    if (auto value = T{}; tr_variantGetReal(variant, &value))
-    {
-        ret = value;
-    }
-
-    return ret;
-}
-
-template<typename T>
-auto getValue(tr_variant const* variant)
-    requires std::is_same_v<T, QString>
-{
-    std::optional<T> ret;
-
-    if (auto sv = std::string_view{}; tr_variantGetStrView(variant, &sv))
-    {
-        ret = QString::fromUtf8(std::data(sv), static_cast<IF_QT6(qsizetype, int)>(std::size(sv)));
-    }
-
-    return ret;
-}
-
-template<typename T>
-auto getValue(tr_variant const* variant)
-    requires std::is_same_v<T, std::string_view>
-{
-    std::optional<T> ret;
-
-    if (auto sv = std::string_view{}; tr_variantGetStrView(variant, &sv))
-    {
-        ret = std::string_view(std::data(sv), std::size(sv));
-    }
-
-    return ret;
-}
-
-template<typename T>
-auto getValue(tr_variant const* variant)
-    requires std::is_enum_v<T>
-{
-    std::optional<T> ret;
-
-    if (auto const value = getValue<int>(variant); value)
-    {
-        ret = static_cast<T>(*value);
-    }
-
-    return ret;
-}
-
-template<typename C, typename T = typename C::value_type>
-auto getValue(tr_variant const* var)
-    requires std::is_same_v<C, QStringList> || std::is_same_v<C, QList<T>> || std::is_same_v<C, std::vector<T>>
-{
-    std::optional<C> ret;
-
-    if (var != nullptr && var->holds_alternative<tr_variant::Vector>())
-    {
-        auto list = C{};
-
-        for (size_t i = 0, n = tr_variantListSize(var); i < n; ++i)
-        {
-            tr_variant* const child = tr_variantListChild(const_cast<tr_variant*>(var), i);
-            auto const value = getValue<T>(child);
-            if (value)
-            {
-                list.push_back(*value);
-            }
-        }
-
-        ret = list;
     }
 
     return ret;
